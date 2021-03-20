a781746ee48179efc9b5bc1d4eec1b21422f0bb5
Mike <bjens13@student.sdu.dk>
2021-03-20 09:56:43 +0100

    Initial commit

---
+++ GCF assignment/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace GCF_assignment
+{
+    public class Program
+    {
+        public static int GCD(int arg1, int arg2) 
+        {
+            // If arg2 is exactly 0 we know that we have found the GCD and the code will return input1 as the answer.
+            if (arg2 == 0)
+            {
+                return arg1;
+            }
+
+            /* If arg2 is not exactly 0 we run 'GCD' recursively until it is. 
+             * We do that by returning the recursive modulo of the 2 inputs until the condition is met. */
+            else
+            {
+                return GCD(arg2, arg1 % arg2);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // Intro
+            Console.WriteLine("This program gives you the Greatest Common Factor of 2 numbers");
+            Console.WriteLine("");
+            
+            // Label used in 'goto' on line 45
+            Input1:
+            Console.WriteLine("Please input the first number: ");
+
+            string Input1 = Console.ReadLine();
+            int one;
+
+            // Checks if input is valid or not
+            if (!int.TryParse(Input1, out one))
+            {
+                Console.WriteLine("Not a valid input, try again.");
+                Console.WriteLine("");
+                goto Input1;
+            }
+
+            else if (one == 0)
+            {
+                Console.WriteLine("Input can't be 0. Please input numbers higher than 0");
+                goto Input1;
+            }
+
+            // Label used in 'goto' on line 66
+            Input2:
+            Console.WriteLine("Please input the second number: ");
+
+            string Input2 = Console.ReadLine();
+            int two;
+
+            // Checks if input is valid or not
+            if (!int.TryParse(Input2, out two))
+            {
+                Console.WriteLine("Not a valid input, try again.");
+                Console.WriteLine("");
+                goto Input2;
+            }
+
+            else if (two == 0)
+            {
+                Console.WriteLine("Input can't be 0. Please input numbers higher than 0");
+                goto Input2;
+            }
+
+            int output = GCD(one, two);
+            Console.WriteLine("The GCF is: " + output);
+            Console.ReadLine();
+        }
+
+
+    }
+}
---
+++ UnitTestProject2/ProgramTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GCF_assignment;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GCF_assignment.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [DataTestMethod()]
+        [DataRow(10, 5)]     // Positive numbers only   // Expected(5, 5)
+
+        public void GCDTest1(int arg1, int arg2)
+        {
+            int PNO = Program.GCD(arg1, arg2);
+            Assert.AreEqual(PNO, 5);
+        }
+
+        [DataTestMethod()]
+        [DataRow(-10, -5)]   // Negative numbers only   // Expected(-5, -5)
+
+        public void GCDTest2(int arg1, int arg2)
+        {
+            int NNO = Program.GCD(arg1, arg2);
+            Assert.AreEqual(NNO, -5);
+        }
+
+        [DataTestMethod()]
+        [DataRow(10, -5)]   // Positive number and negative number   // Expected(-5, -5)
+
+        public void GCDTest3(int arg1, int arg2)
+        {
+            int NNO = Program.GCD(arg1, arg2);
+            Assert.AreEqual(NNO, -5);
+        }
+    }
+}
